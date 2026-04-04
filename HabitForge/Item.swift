db8ae9e1665ef554253a89e70afdefaf856b8efc
Praveet Gupta <praveetgupta@gmail.com>
2026-04-05 01:22:28 +0530

    Initial Commit

---
+++ HabitForge/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HabitForge
+//
+//  Created by Praveet Gupta on 05/04/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ HabitForgeTests/HabitForgeTests.swift
@@ -0,0 +1,19 @@
+//
+//  HabitForgeTests.swift
+//  HabitForgeTests
+//
+//  Created by Praveet Gupta on 05/04/26.
+//
+
+import Testing
+@testable import HabitForge
+
+struct HabitForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
